5abf1b521f763b3244b43d2611f3a23a31387192
Tswank1 <143465792+Tswank1@users.noreply.github.com>
2025-03-05 22:12:27 -0800

    Update Heaps.py
    Fixed issue where B and K would be incorrectly estimated

--- Code/CodePart3/Heaps.py
+++ Code/CodePart3/Heaps.py
@@ -1,24 +1,27 @@
 import matplotlib.pyplot as plt
 import sys
 import math
+import os
 
-fileName = sys.argv[1]
+directory = sys.argv[1]
+docNum = sys.argv[2]
 
 uniqueWords = []
 vocabulary = []
 
-with open(fileName, 'r') as file:
-    for line in file:
-        if line not in uniqueWords:
-            uniqueWords.append(line)
-        vocabulary.append(len(uniqueWords))
+for fileName in os.listdir(directory):
+    with open(directory + '\\' + fileName, 'r') as file:
+        for line in file:
+            if line not in uniqueWords:
+                uniqueWords.append(line)
+            vocabulary.append(len(uniqueWords))
 
 
 #heaps vals
 index1 = (int)(len(vocabulary) * .3)
 index2 = (int)(len(vocabulary) * .65)
 B = (math.log(vocabulary[index1], 10) - math.log(vocabulary[index2], 10)) / (math.log(index1, 10) - math.log(index2, 10))
-k = round(math.log(vocabulary[index2], 10) / (B * math.log(index2, 10)), 3)
+k = round(vocabulary[index1] / (pow(index1, B)), 3)
 B = round(B, 3)
 
 print("Corpus: ", len(vocabulary), " UniqueWords: ", len(uniqueWords))
@@ -29,11 +32,11 @@ Heaps = [k * pow(x, B) for x in range(len(vocabulary))]
 lbl = "Heaps " + str(k) + ", " + str(B)
 plt.plot(Heaps, label = lbl, ls = ':')
 
-plt.plot(vocabulary, label = fileName, color = 'blue')
+plt.plot(vocabulary, label = "Crawl" + str(docNum), color = 'blue')
 
 plt.xlabel('Words in Collection')
 plt.ylabel('Words in Vocabulary')
-plt.title("Vocabular growth for " + fileName)
+plt.title("Vocabular growth for Crawl" + str(docNum))
 plt.legend()
 
 plt.show()
