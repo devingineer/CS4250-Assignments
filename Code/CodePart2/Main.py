30d14e167f8dfdb5e312734d79e2d4602a7b6b50
bearismybrother <thetwai123456789@gmail.com>
2025-03-05 21:22:14 -0800

    Added repo, part 2 outputs, and fixed bugs in part 2 code

--- Code/CodePart2/Main.py
+++ Code/CodePart2/Main.py
@@ -3,26 +3,29 @@ from Tokenizer import tokenize_document
 from Stemmer import stem_tokens
 
 # Update this path to the actual folder containing HTML files
-directory = os.path.join(os.getcwd(), "Code", "CodePart2", "ExamplePage")
+directory = os.path.join(os.getcwd(), "repository", )
 
 # Ensure output directories exist
-output_dir = os.path.join(os.getcwd(), "Code", "CodePart2", "Output")
-os.makedirs(output_dir, exist_ok=True)
+output_dir = os.path.join(os.getcwd(), "Output", "OutputPart2")
 
-for filename in os.listdir(directory):
-    file_path = os.path.join(directory, filename)
-    
-    # Process only HTML files
-    if file_path.endswith(".html"):
-         with open(file_path, "r", encoding="utf-8") as handle:
-                # Tokenize
-                tokenized_words = tokenize_document(handle.read())
+for repo in os.listdir(directory):
+    repo_path = os.path.join(directory, repo)
+    for filename in os.listdir(repo_path):
+        file_path = os.path.join(repo_path, filename)
+        
+        # Process only HTML files
+        if file_path.endswith(".html"):
+            with open(file_path, "r", encoding="utf-8") as handle:
+                    # Tokenize
+                    tokenized_words = tokenize_document(handle.read())
 
-                # Stemming
-                stemmed_words = stem_tokens(tokenized_words) # Apply stemming to the full list
+                    # Stemming
+                    stemmed_words = stem_tokens(tokenized_words) # Apply stemming to the full list
 
-                # Save output
-                base_filename = os.path.splitext(filename)[0] # Remove extension from filename
-                output_path = os.path.join(output_dir, f"{base_filename}-StemmedOutput.txt")
-                with open(output_path, "w", encoding="utf-8") as file:
-                    file.write("\n".join(stemmed_words))  # Write all words at once
+                    # Save output
+                    base_filename = os.path.splitext(filename)[0] # Remove extension from filename
+                    output_repo_path = os.path.join(output_dir, repo)
+                    os.makedirs(output_repo_path, exist_ok=True)
+                    output_path = os.path.join(output_repo_path, f"{base_filename}-StemmedOutput.txt")
+                    with open(output_path, "w", encoding="utf-8") as file:
+                        file.write("\n".join(stemmed_words))  # Write all words at once
