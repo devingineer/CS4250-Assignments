d9f079e7f0865f88cd55afdc65bcfcd96b6d83c1
Devin Khun <lightningowl2004@gmail.com>
2025-05-04 17:22:21 -0700

    feat: simple boolean retrieval function

--- Assignment2/boolean_retrieval.py
+++ Assignment2/boolean_retrieval.py
@@ -9,3 +9,26 @@ def load_index(json_filename="inverted_index.json"):
     with open(json_path, "r", encoding="utf-8") as f:
         index = json.load(f)
     return index
+
+
+def boolean_and_search(index, query):
+    query_words = re.findall(r'\w+', query.lower())
+
+    if not query_words:
+        return []
+
+    # Start with the doc list of the first query word
+    first_word = query_words[0]
+    if first_word not in index:
+        return []
+
+    result_docs = set(index[first_word].keys())
+
+    # Intersect with other word results
+    for word in query_words[1:]:
+        if word in index:
+            result_docs &= set(index[word].keys())
+        else:
+            return []  # If any word isn't in the index, no doc will match all
+
+    return sorted(result_docs)
